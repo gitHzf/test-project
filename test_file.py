bde2b8ec33e8a1177f7a470b1a38737d643be173
hzf <188230163@qq.com>
2026-09-08 14:15:28 +0800

    aa

--- test_file.py
+++ test_file.py
@@ -1,3 +1,13 @@
 # 目标：测试输出第一个python应用程序并上传到github。
 # 在vs code中运行此程序，查看输出结果。
-print("Upload to github!")
+print("Upload to github!");
+x,y=1,2;
+print(f'x={x} y={y}');
+a0=1
+print(a0)
+NULL=None
+print(f'NULL={NULL}')
+str='hello world \"hello\"'
+print(f'str={str}')
+print(str)
+
